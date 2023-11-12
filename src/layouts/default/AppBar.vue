38a3b4b76606b9aac53dd494d46086fc67189175
Erick <erick@Erick-Nitro.localdomain>
2023-11-12 14:09:44 -0300

    Merge branch 'main' from remote

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar app flat color="primary">
+    <v-img src="@/assets/VagaCerta.png" max-height="120" class="mr-2"></v-img>
+    <v-btn text to="/vagas">
+      Vagas
+    </v-btn>
+    <v-btn text to="/empresas">
+      Empresas
+    </v-btn>
+    <v-btn text to="/candidato">
+      Candidato
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text icon color="black" variant="flag">
+      <v-icon>mdi-account-plus</v-icon>
+     <!--TEXTO POREM TA BUGADO-->
+     teste
+    </v-btn>
+    <v-btn text icon color="black" variant="flag">
+      <v-icon>mdi-login</v-icon>
+      teste
+    <!--TEXTO POREM TA BUGADO-->
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script setup>
+//
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <HelloWorld />
+  <v-btn>
+    erickkk gameplay
+  </v-btn>
+</template>
+
+<script lang="ts" setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+
+</script>
